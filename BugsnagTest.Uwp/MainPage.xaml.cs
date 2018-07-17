d6e0046e7d4698f17965794e27a49f198f3ff35a
Andrew Grebennikov <av.grebennikov@gmail.com>
2018-07-17 11:11:37 -0400

    initial commit

---
+++ BugsnagTest.Uwp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BugsnagTest.Uwp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            throw new Exception("Hi! I'm unhandled exception.");
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Details
+This project illustrates issue reported in https://github.com/bugsnag/bugsnag-dotnet/issues/107
+
+As bugsnag references netstandard2 the only way to make it work is to set Windows 10, version 1803 (Build 17134) as Target Windows version.
